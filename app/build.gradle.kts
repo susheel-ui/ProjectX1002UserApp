c58fc062d5ec6b0480fc723dfb20e0d251576ba2
Susheel Verma <72841849+susheel-ui@users.noreply.github.com>
2025-12-02 00:04:43 +0530

    V1_withBugs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,9 +61,6 @@ dependencies {
     implementation("androidx.viewpager2:viewpager2:1.1.0")
 
     // optional, remove if unused
-    implementation ("ua.naiksoftware:stomp-websocket:1.6.6") // or the version you had
-    implementation ("com.squareup.okhttp3:okhttp:4.11.0") // for OkHttp provider used by STOMP
-    implementation ("androidx.work:work-runtime-ktx:2.9.2")
 
     //retrofit
     implementation(libs.retrofit)
